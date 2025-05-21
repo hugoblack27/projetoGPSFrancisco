06fd3b5c1c144f1db81252d0113b8685b2b0d163
hugoblack27 <hugohenrique9341@gmail.com>
2025-05-21 10:52:04 -0300

    Update e delete

---
+++ GPSFrancisco/frmMenuPrincipal.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace GPSFrancisco
+{
+    partial class frmMenuPrincipal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMenuPrincipal));
+            this.btnVoluntarios = new System.Windows.Forms.Button();
+            this.btnVoltar = new System.Windows.Forms.Button();
+            this.btnUsuarios = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnVoluntarios
+            // 
+            this.btnVoluntarios.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnVoluntarios.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnVoluntarios.Image = ((System.Drawing.Image)(resources.GetObject("btnVoluntarios.Image")));
+            this.btnVoluntarios.Location = new System.Drawing.Point(24, 11);
+            this.btnVoluntarios.Name = "btnVoluntarios";
+            this.btnVoluntarios.Size = new System.Drawing.Size(154, 222);
+            this.btnVoluntarios.TabIndex = 0;
+            this.btnVoluntarios.Text = "V&oluntários";
+            this.btnVoluntarios.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.btnVoluntarios.UseVisualStyleBackColor = true;
+            // 
+            // btnVoltar
+            // 
+            this.btnVoltar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnVoltar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnVoltar.Image = ((System.Drawing.Image)(resources.GetObject("btnVoltar.Image")));
+            this.btnVoltar.Location = new System.Drawing.Point(686, 503);
+            this.btnVoltar.Name = "btnVoltar";
+            this.btnVoltar.Size = new System.Drawing.Size(86, 46);
+            this.btnVoltar.TabIndex = 1;
+            this.btnVoltar.Text = "&Voltar";
+            this.btnVoltar.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnVoltar.UseVisualStyleBackColor = true;
+            this.btnVoltar.Click += new System.EventHandler(this.btnVoltar_Click);
+            // 
+            // btnUsuarios
+            // 
+            this.btnUsuarios.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnUsuarios.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUsuarios.Image = ((System.Drawing.Image)(resources.GetObject("btnUsuarios.Image")));
+            this.btnUsuarios.Location = new System.Drawing.Point(196, 11);
+            this.btnUsuarios.Name = "btnUsuarios";
+            this.btnUsuarios.Size = new System.Drawing.Size(154, 222);
+            this.btnUsuarios.TabIndex = 2;
+            this.btnUsuarios.Text = "&Usuários";
+            this.btnUsuarios.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.btnUsuarios.UseVisualStyleBackColor = true;
+            this.btnUsuarios.Click += new System.EventHandler(this.btnUsuarios_Click);
+            // 
+            // frmMenuPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.btnUsuarios);
+            this.Controls.Add(this.btnVoltar);
+            this.Controls.Add(this.btnVoluntarios);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "frmMenuPrincipal";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "GPSFrancisco - Menu Principal";
+            this.Load += new System.EventHandler(this.frmMenuPrincipal_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnVoluntarios;
+        private System.Windows.Forms.Button btnVoltar;
+        private System.Windows.Forms.Button btnUsuarios;
+    }
+}
---
+++ dbfrancisco.sql
@@ -0,0 +1,33 @@
+drop database dbFrancisco;
+
+create database dbFrancisco;
+
+use dbFrancisco;
+
+create table tbUsuarios(
+codUsu int not null auto_increment,
+nome varchar(50) not null,
+senha varchar(12) not null,
+primary key(codUsu));
+
+insert into tbUsuarios(nome,senha)
+	values('sfrancisco','123456');
+
+select * from tbUsuarios;
+
+select nome,senha from tbUsuarios 
+where nome='admin' and senha='admin';
+
+select nome from tbusuarios order by nome asc;
+
+
+update tbUsuarios set nome = 'senac', senha = '123456789123' where codUsu = 1;
+
+-- pesquisa filtrada por codigo
+
+select * from tbusuarios where codusu = codusu;
+
+-- pesquisa filtrada por nome
+
+select * from tbUsuarios where nome like '%nome%';
+
